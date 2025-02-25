a5ba63fe20fa26d3ab60ea91236c7c4e72ae2c0f
Jojoussef <101554960+Jojoussef@users.noreply.github.com>
2025-02-25 20:51:08 +0100

    refractor : remove unused imports

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,3 @@
-import { siteConfig } from "@/config/site";
-import { buttonVariants } from "@/components/ui/button";
-
 export default function HomePage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
