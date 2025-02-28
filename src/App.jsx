f8cc96977411ddc16fb4d3b9946d04d84e69e73b
Jojoussef <101554960+Jojoussef@users.noreply.github.com>
2025-03-01 00:40:38 +0100

    feat: add routes for Chatbot and TodoList pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
 import HomePage from "@/pages/HomePage";
+import Chatbot from "./pages/Chatbot";
+import TodoList from "./pages/TodoList";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Layout>
         <Routes>
           <Route exact path="/" Component={HomePage} />
+          <Route exact path="/todo" Component={TodoList} />
+          <Route path="/chatbot" Component={Chatbot} />
         </Routes>
       </Layout>
     </BrowserRouter>
