bbf4c34bd2e32b541eb7537e04b6b6e6390db382
Jojoussef <101554960+Jojoussef@users.noreply.github.com>
2025-03-01 00:40:51 +0100

    feat: integrate random quote API into HomePage component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,32 @@
+import { useEffect, useState } from "react";
+import { getRandomQuote } from "../api/quotes";
+
 export default function HomePage() {
+  const [quote, setQuote] = useState({
+    id: 0,
+    content: "",
+    url: "",
+    language_code: "fr",
+    originator: {
+      id: 0,
+      name: "",
+    },
+    tags: [],
+  });
+  useEffect(() => {
+    getRandomQuote().then((results) => {
+      setQuote(results);
+    });
+  }, []);
+
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Welcome To My Personal Page
+        <h1 className="text-3xl  text-[#d000ff] font-extrabold leading-tight tracking-tighter md:text-4xl">
+          {quote.content}
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
-          This is a small paragraph describing the content of my personal page.
-          Here you can find information about my projects, blog posts, and more.
+          {quote.originator.name}
         </p>
       </div>
     </section>
